ed5d1f4bcd61de516e21fd9e3bec35cf3966a821
hyuga <hyuga3110@gmail.com>
2018-05-19 23:34:08 +0900

    create home view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
-  root :to => 'page_tops#index'
-  get 'users/new'
+  
+  get 'sessions/new'
+  # root :to => 'static_pages#home'
+  root :to => 'sessions#new'
+  # get 'users/new'
+  get 'static_pages/about',as: 'about'
+  get 'static_pages/help',as: 'help'
+  get 'static_pages/home',as: 'home'
   get '/signup', to: 'users#new'
-  resources :friends
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  resources :users
+  # resources :friends
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_12_114847) do
+ActiveRecord::Schema.define(version: 2018_05_18_152842) do
 
   create_table "friends", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,9 @@ ActiveRecord::Schema.define(version: 2018_05_12_114847) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.boolean "admin", default: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
